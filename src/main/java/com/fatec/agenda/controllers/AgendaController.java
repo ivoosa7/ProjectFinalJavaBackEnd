d30e442924b7ac91b76413efe222e1d93f3879e7
Ivo Araujo <ivo.souza-23@outlook.com>
2024-12-10 11:38:23 -0300

    Todos os serviços criados com seus respectivos endPoints

--- src/main/java/com/fatec/agenda/controllers/AgendaController.java
+++ src/main/java/com/fatec/agenda/controllers/AgendaController.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatec.agenda.dtos.AgendaRequest;
 import com.fatec.agenda.dtos.AgendaResponse;
 import com.fatec.agenda.service.AgendaService;
 
@@ -22,6 +28,33 @@ public class AgendaController {
     @GetMapping
     public ResponseEntity<List<AgendaResponse>> getAgenda(){
         return ResponseEntity.ok(service.getAllAgendas());
+    }
+
+    //2 EndPoint
+    @GetMapping("{id}")
+    public ResponseEntity<AgendaResponse> getAgenda(@PathVariable long id){
+        return ResponseEntity.ok(service.getAgendaById(id));
+    }
+
+    //3 EndPoint
+    @PostMapping()
+    public ResponseEntity<AgendaResponse> saveNewAgenda(@RequestBody AgendaRequest agenda){
+        AgendaResponse newAgenda = service.saveAgenda(agenda);
+        return ResponseEntity.created(null).body(newAgenda);
+    }
+
+    //4 EndPoint
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateAgenda(@PathVariable long id, @RequestBody AgendaRequest agenda){
+        service.updateAgenda(agenda, id);
+        return ResponseEntity.ok().build();
+    }
+
+    //5 EndPoint
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteAgenda(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
     
 }
--- src/main/java/com/fatec/agenda/service/AgendaService.java
+++ src/main/java/com/fatec/agenda/service/AgendaService.java
@@ -6,9 +6,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.agenda.dtos.AgendaRequest;
 import com.fatec.agenda.dtos.AgendaResponse;
+import com.fatec.agenda.entities.Agenda;
 import com.fatec.agenda.mappers.AgendaMapper;
 import com.fatec.agenda.repositories.AgendaRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AgendaService {
@@ -16,7 +20,45 @@ public class AgendaService {
     @Autowired
     private AgendaRepository repository;
 
+    //1 Service
     public List <AgendaResponse> getAllAgendas(){
         return repository.findAll().stream().map(p -> AgendaMapper.toDTO(p)).collect(Collectors.toList());
+    }
+
+    //2 Service
+    public AgendaResponse getAgendaById(long id){
+        Agenda agenda = repository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Não há agendamento")
+        );
+
+        return AgendaMapper.toDTO(agenda);
+    }
+
+    //3 Service 
+    public AgendaResponse saveAgenda(AgendaRequest agenda){
+        Agenda newAgenda = repository.save(AgendaMapper.toEntity(agenda));
+        return AgendaMapper.toDTO(newAgenda);
+    }
+
+    //4 Service
+    public void updateAgenda (AgendaRequest agenda, long id){
+        Agenda aux = repository.getReferenceById(id);
+
+        aux.setTitulo(agenda.titulo());
+        aux.setDescricao(agenda.descricao());
+        aux.setData(agenda.data());
+        aux.setHora(agenda.hora());
+        aux.setLocal(agenda.local());
+
+        repository.save(aux);
+    }
+
+    //5 Service
+    public void delete(long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }else{
+            throw new EntityNotFoundException("Compromisso não marcado");
+        }
     }
 }
